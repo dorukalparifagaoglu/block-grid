e372434a71a7e5e7129012223b755b71e05f4a39
Doruk Alp Arifagaoglu <arifagaogludorukalp@gmail.com>
2025-07-15 00:34:54 +0300

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,48 @@
+let distMouse = 15;
+let b; let size = 10; let cols; let rows; let offset = 4;
+let blocks = [];
+
+
+
+function setup() {
+  createCanvas(400, 400);
+  rectMode(CENTER);
+  angleMode(DEGREES);
+  cols = width/size;
+  rows = height/size;
+  
+  
+  
+  for (let i=0; i<cols; i++) {
+    blocks[i]= [];
+    for (let j=0; j<rows; j++) {
+      blocks[i][j] = new Block(size/2 + i*size, size/2 + j*size);
+      
+    }
+  }
+  
+  
+  
+
+}
+
+function draw() {
+  background(0);
+  
+  for (let i=0; i<cols; i++) {
+    for (let j=0; j<rows; j++) {
+      blocks[i][j].move();
+      blocks[i][j].display();
+    }
+  }
+  
+   
+  
+  }
+  
+  
+  
+  
+  
+  
+ 
